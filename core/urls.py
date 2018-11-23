ed34d9cf70bd57b7b52c60aae64ca20d840aa5d8
vihar <vihar.kurama@gmail.com>
2018-11-24 00:19:42 +0530

    url-change

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', StatusView.as_view(), name='status'),
     path('items/<pk>/', ItemDetailEndpoint.as_view(), name='item-list'),
     path('item/create/', ItemCreate.as_view(), name='item-create'),
-    path('item-list', ItemListAPIView.as_view(), name='item-list'),
+    path('item-list/', ItemListAPIView.as_view(), name='item-list'),
     path('social-login/', SocialLoginView.as_view(), name='social-login')
 ]
