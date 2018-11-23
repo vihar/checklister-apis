9547df0c36d6730b85cbff15086e8ad8a21ce257
vihar <vihar.kurama@gmail.com>
2018-11-23 23:52:30 +0530

    cors

--- checklister/settings/common.py
+++ checklister/settings/common.py
@@ -44,8 +44,10 @@ CORS_ORIGIN_WHITELIST = (
     '127.0.0.1:9000',
     'localhost:8080',
     'localhost:8081',
-    'checklister-api.herokuapp.com'
+    'checklister-api.herokuapp.com',
 )
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
